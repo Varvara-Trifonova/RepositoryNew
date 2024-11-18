2258e5dab391fda1a1007c4ec0bea053bebadf22
Varya <trifonovavarya@gmail.com>
2024-11-18 14:46:01 +0500

    2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pryamo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Введите длину первого катета:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите длину второго катета:");
+            double b = double.Parse(Console.ReadLine());
+
+            double c = Math.Sqrt(a * a + b * b);
+
+            double height = (a * b) / c;
+
+            Console.WriteLine($"Длина высоты, опущенной из вершины прямого угла на гипотенузу: {height}");
+
+            Console.ReadKey();
+        }
+    }
+}
