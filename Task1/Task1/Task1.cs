1d34367d6f56831ee0ffddaf70b8f44025bf47f5
Varya <trifonovavarya@gmail.com>
2024-11-18 13:46:41 +0500

    Task1.1

---
+++ Task1/Task1/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Life");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Rapidly, merrily,");
+            Console.WriteLine("Life’s sunny hours flit by,");
+            Console.WriteLine("Gratefully, cheerily, ha");
+            Console.WriteLine("Enjoy them as they fly!");
+
+            Console.ResetColor();
+
+
+            Console.ReadKey();
+        }
+    }
+}
