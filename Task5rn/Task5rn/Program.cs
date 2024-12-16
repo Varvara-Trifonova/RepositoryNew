5c543e7f59b4fa69ad29c95fe189aa7dc2638553
Varya <trifonovavarya@gmail.com>
2024-12-16 13:40:54 +0500

    Task5

---
+++ Task5rn/Task5rn/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = F(1, 1) + F(1, 2) + F(5, 3);
+
+            Console.WriteLine($"x = {x:F3}");
+            Console.ReadKey();
+        }
+        static double F(double a, double b)
+        {
+            return (1 /( b + (a / (b + (a / (b + Math.Sqrt(a)))))));
+        }
+    }
+}
