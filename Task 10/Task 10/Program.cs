66be0c1081717a4c5ee6fbcf523a0e5bd7d1a3cb
Varya <trifonovavarya@gmail.com>
2025-01-19 22:23:46 +0500

    1 готово

---
+++ Task 10/Task 10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите число k (2 <= k <= 9): ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (k < 2 || k > 9)
+            {
+                Console.WriteLine("число должно быть в диапазоне от 2 до 9");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine($"Таблица умножения на {k}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{k} * {i} = {k * i}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
