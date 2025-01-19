681968122e590ccf6f546c52ebcd8b58abdcef7e
Varya <trifonovavarya@gmail.com>
2025-01-19 22:46:03 +0500

    2

--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -4,27 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace task10_1
+namespace task10_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.Write("Введите число k (2 <= k <= 9): ");
-            int k = int.Parse(Console.ReadLine());
+            Console.Write("Введите натуральное число n: ");
+            int n = int.Parse(Console.ReadLine());
 
-            if (k < 2 || k > 9)
+            if (n < 1)
             {
-                Console.WriteLine("число должно быть в диапазоне от 2 до 9");
+                Console.WriteLine("число n должно быть больше или равно 1");
                 Console.ReadKey();
                 return;
             }
 
-            Console.WriteLine($"Таблица умножения на {k}:");
-            for (int i = 1; i <= 10; i++)
+            double[] a = new double[n + 1];
+
+            for (int i = 0; i <= n; i++)
             {
-                Console.WriteLine($"{k} * {i} = {k * i}");
+                Console.Write($"Введите a{i}: ");
+                a[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Последовательность сумм соседних элементов:");
+            for (int i = 0; i < n; i++)
+            {
+                double sum = a[i] + a[i + 1];
+                Console.WriteLine($"a{i} + a{i + 1} = {sum}");
                 Console.ReadKey();
             }
         }
