17bd6576121b84a6825bf378274d08c4d32f6c12
Varya <trifonovavarya@gmail.com>
2025-01-20 16:44:35 +0500

    12готово

---
+++ Tasr12NewNew/Tasr12NewNew/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12NewNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Random rand = new Random();
+
+            Console.Write("Введите число m (5 <= m <= 20): ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите число n (5 <= n <= 20): ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (m < 5 || m > 20 || n < 5 || n > 20)
+            {
+                Console.WriteLine("Одно или оба значения не соответствуют условиям 5 <= m, n <= 20");
+                Console.ReadKey();
+                return;
+            }
+
+            int[,] array = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = rand.Next(0, 100);
+                }
+            }
+
+            Console.WriteLine("Массив:");
+            PrintArray(array);
+
+            Console.Write("Введите число для проверки: ");
+            int checkNumber = int.Parse(Console.ReadLine());
+
+            if (HasElementGreaterThan(array, checkNumber, out int row, out int col))
+            {
+                Console.WriteLine($"Найден элемент больше {checkNumber} на позиции: строка {row}, столбец {col}");
+            }
+            else
+            {
+                Console.WriteLine($"Элементов больше {checkNumber} в массиве нет");
+            }
+
+            double[] averages = ColumnAverages(array);
+            Console.WriteLine("Среднее арифметическое для каждого столбца:");
+            for (int i = 0; i < averages.Length; i++)
+            {
+                Console.WriteLine($"Столбец {i}: {averages[i]:F2}");
+            }
+            Console.ReadKey();
+        }
+        static void PrintArray(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{array[i, j],4} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool HasElementGreaterThan(int[,] array, int number, out int row, out int col)
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+
+            for (row = 0; row < rows; row++)
+            {
+                for (col = 0; col < cols; col++)
+                {
+                    if (array[row, col] > number)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            row = -1;
+            col = -1;
+            return false;
+        }
+
+        static double[] ColumnAverages(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+            double[] averages = new double[cols];
+
+            for (int col = 0; col < cols; col++)
+            {
+                double sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += array[row, col];
+                }
+                averages[col] = sum / rows;
+            }
+
+            return averages;
+        }
+    }
+}
---
+++ Tasr12NewNew/Tasr12NewNew/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Tasr12NewNew")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tasr12NewNew")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e318f2b4-959d-4ddc-8374-72f92378b14b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
